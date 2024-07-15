57f9199c26aa785adc28e68546119368ad0dbd03
Allils <168203141+Allils@users.noreply.github.com>
2024-07-16 02:36:10 +0300

    Add files via upload

---
+++ lLibrary/DisplayInfo.cs
@@ -0,0 +1,21 @@
+﻿using InfoAboutBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class DisplayInfo
+    {
+        public void DisplayBooks(List<Book> bookList)
+        {
+            foreach (var book in bookList)
+            {
+                Console.WriteLine($"Title - {book.Title} \t Genre - ({book.Genre}) \t Author - {book.Author} \t Year- {book.Year}");
+            }
+        }
+    }
+
+}
